e71aad83498600b778527c55f3e898d620405c83
DeepthiValachery <deepthi.valachery@my.uwi.edu>
2024-02-21 02:47:46 +0000

    Created in the User_Pokemon

--- App/models.py
+++ App/models.py
@@ -3,11 +3,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class UserPokemon(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
+  id = db.Column('id', db.Integer, primary_key=True)
+  userId = db.Column('user_ID', db.Integer, db.ForeignKey(user.id))
+  pokeId = db.Column('pokemon_ID', db.Integer, db.ForeignKey(Pokemon.pokeId))
+  name = db.Column(db.String(50))
+  pokemen = db.relationship('Pokemon')
   pass
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+
   pass
 
 class Pokemon(db.Model):
