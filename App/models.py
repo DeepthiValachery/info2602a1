a4299b83ef2a4ca783507074880c90a9089f4a61
DeepthiValachery <deepthi.valachery@my.uwi.edu>
2024-02-21 23:45:40 +0000

    database

--- App/models.py
+++ App/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
 db = SQLAlchemy()
 
 class UserPokemon(db.Model):
@@ -8,6 +9,12 @@ class UserPokemon(db.Model):
   pokeid = db.Column('pokeid', db.Integer, db.ForeignKey('Pokemon.pokeid'))
   name = db.Column(db.String(50))
   pokemen = db.relationship('Pokemon')
+
+  def toDict(self):
+    return{
+      'name':self.name,
+      'stats':self.pokemon.toDict()
+    }
   pass
 
 class User(db.Model):
@@ -15,6 +22,14 @@ class User(db.Model):
   username = db.Column('username', db.String(80), unique=True, nullable=False)
   email = db.Column('email', db.String(120), unique=True, nullable=False)
   password = db.Column('password', db.String(120), unique=True, nullable=False)
+
+  def toDict(self):
+    return {
+      "id": self.id,
+      "username": self.username,
+      "email": self.email,
+      "password": self.password
+    }
 
   #must have passwordSet, passwordCheck
   pass
@@ -32,4 +47,20 @@ class Pokemon(db.Model):
   weight = db.Column('weight', db.Integer, nullable=True)
   type1 = db.Column('type1', db.String(50), nullable=False)
   type2 = db.Column('type2', db.String(50), nullable=True)
+  
+  def toDict(self):
+    return {
+      "pokeid": self.pokeid,
+      "name": self.name,
+      "attack": self.attack,
+      "defense": self.defense,
+      "hp": self.hp,
+      "height": self.height,
+      "sp_attack": self.sp_attack,
+      "sp_defense": self.sp_defense,
+      "speed": self.speed,
+      "weight": self.weight,
+      "type1": self.type1,
+      "type2": self.type2
+    }
   pass
