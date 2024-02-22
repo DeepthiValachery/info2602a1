fbb931c999acea20035993cecbd5dc1dd36ded4b
DeepthiValachery <deepthi.valachery@my.uwi.edu>
2024-02-22 00:04:03 +0000

    db init pt2

--- App/app.py
+++ App/app.py
@@ -1,5 +1,7 @@
 import os, csv
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask.cli import with_appcontext
 from functools import wraps
 from flask_cors import CORS
 from sqlalchemy.exc import IntegrityError
