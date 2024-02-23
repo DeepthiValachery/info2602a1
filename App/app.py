ec17618cac52f464ddc82781e28aca933ec1e81d
DeepthiValachery <deepthi.valachery@my.uwi.edu>
2024-02-23 01:40:41 +0000

    ugi

--- App/app.py
+++ App/app.py
@@ -198,5 +198,5 @@ def delete(n):
     return jsonify(error='Error releasing Pokemon'), 500
 
   if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+    app.run(host='0.0.0.0', port=8080)
 
