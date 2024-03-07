1a830a7756cf2015d61ec55edcacf6efdb2632c9
dj_Chery <122883959+DeepthiValachery@users.noreply.github.com>
2024-03-07 10:54:40 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,202 +1,287 @@
 import os, csv
-from flask import Flask, jsonify, request, Response
-from flask_sqlalchemy import SQLAlchemy
-from flask.cli import with_appcontext
+from flask import Flask, jsonify, request
 from functools import wraps
 from flask_cors import CORS
 from sqlalchemy.exc import IntegrityError
-from werkzeug.security import check_password_hash
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
     get_jwt_identity,
     jwt_required,
-    current_user,
     set_access_cookies,
     unset_jwt_cookies,
 )
 
-from models import db, User, UserPokemon, Pokemon
+from .models import db, User, UserPokemon, Pokemon
 
 # Configure Flask App
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'MySecretKey'
-app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
-app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "MySecretKey"
+app.config["JWT_ACCESS_COOKIE_NAME"] = "access_token"
+app.config["JWT_REFRESH_COOKIE_NAME"] = "refresh_token"
 app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
 app.config["JWT_COOKIE_SECURE"] = True
 app.config["JWT_SECRET_KEY"] = "super-secret"
 app.config["JWT_COOKIE_CSRF_PROTECT"] = False
-app.config['JWT_HEADER_TYPE'] = ""
-app.config['JWT_HEADER_NAME'] = "Cookie"
+app.config["JWT_HEADER_TYPE"] = ""
+app.config["JWT_HEADER_NAME"] = "Cookie"
 
 
-# Initialize App 
-db = SQLAlchemy(app)
+# Initialize App
 db.init_app(app)
 app.app_context().push()
 CORS(app)
 jwt = JWTManager(app)
 
+
 # Initializer Function to be used in both init command and /init route
 def initialize_db():
-  db.drop_all()
-  db.create_all()
+    db.drop_all()
+    db.create_all()
 
-@app.cli.command('init-db')
-@with_appcontext
-def init_db_command():
-    initialize_db()
-    print('Database initialized!')
+    # Read data from pokemon.csv and create Pokemon objects
+    with open("pokemon.csv", encoding="utf-8") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
 
-def authenticate(username, password):
-  user = User.query.filter_by(username=username).first()
-  if user and user.check_password(password):
-    token = create_access_token(identity=username)
-    response = jsonify(access_token=token)
-    set_access_cookies(response, token)
-    return response
-  return jsonify(message="Invalid username or password"), 401
+            # Check if the height, weight, type2 key exists in the row
+            height = int(row["height"]) if "height" in row and row["height"] else None
 
-# ********** Routes **************
-@app.route('/', methods=['GET'])
-@jwt_required()
-def index():
-  return '<h1>Poke API v1.0</h1>'
+            weight = int(row["weight"]) if "weight" in row and row["weight"] else None
 
-@app.route('/init', methods=['GET'])
-def init_app():
-  initialize_db()
-  ###print('Database initialized!')
-  return jsonify({"message":"Database initialized!"}), 200
+            type2 = row["type2"] if "type2" in row and row["type2"] else None
 
-@app.route('/pokemon', methods=['GET'])
-def listPokemon():
-  allPokemon = Pokemon.query.all()
-  pokemonData = [{"pokemon_id": pokemon.id,
-                  "attack": pokemon.attack,
-                  "defense": pokemon.defense,
-                  "sp_attack": pokemon.sp_attack,
-                  "sp_defense": pokemon.sp_defense,
-                  "speed": pokemon.speed,
-                  "hp": pokemon.hp,
-                  "height": pokemon.height,
-                  "weight": pokemon.weight,
-                  "type1": pokemon.type1,
-                  "type2": pokemon.type2} for pokemon in allPokemon]
-  return jsonify(pokemonData), 200
+            # Create a Pokemon object
+            pokemon = Pokemon(
+                name=row["name"],
+                attack=int(row["attack"]),
+                defense=int(row["defense"]),
+                hp=int(row["hp"]),
+                height=height,
+                sp_attack=int(row["sp_attack"]),
+                sp_defense=int(row["sp_defense"]),
+                speed=int(row["speed"]),
+                weight=weight,
+                type1=row["type1"],
+                type2=type2,
+            )
 
-@app.route('/signup', methods=['POST'])
-def signup():
-  userdata = request.get_json()
-  username = userdata.get('username')
-  email = userdata.get('email')
-  password = userdata.get('password')
-
-  existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
-  if existing_user:
-    return jsonify({"message": "Username or email already exists"}), 409 #Conflict status code
-
-  newuser = User(username=username, email=email)
-  newuser.set_password(password)
-
-  try:
-    db.session.add(newuser)
+            # Add the Pokemon object to the session
+            db.session.add(pokemon)
     db.session.commit()
-    return jsonify({"message": f"{username} created"}), 201 # created status code
-  except IntegrityError:
-    db.session.rollback()
-    return jsonify({"message": "Username or email already exists"}), 409  # 409 Conflict status code
 
-@app.route('/login', methods=['POST'])
-def login():
-  data = request.json
-  username = data.get('username')
-  password = data.get('password')
 
-  user - User.query.filter_by(username=username).first()
-  if not user or not check_password_hash(user.password, password):
-    return jsonify(message='Bad username or password given'), 403   # Forbidden status code
-  
-  access_token = create_access_token(identity=username)
-  return jsonify(access_token=access_token), 200 #OK status code
+def login_user(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        token = create_access_token(identity=username)
+        response = jsonify(access_token=token)
+        set_access_cookies(response, token)
+        return response
+    return jsonify(error="bad username/password given"), 401
 
-@app.route('/mypokemon', methods=['POST'])
+
+# **** Routes ******
+# GET Root
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>Poke API v1.0</h1>"
+
+
+# GET Initialize App
+@app.route("/init", methods=["GET"])
+def init_app():
+    initialize_db()
+    return jsonify({"message": "Database Initialized!"}), 200
+
+
+# GET List Pokemon
+@app.route("/pokemon", methods=["GET"])
+def listPokemon():
+    allPokemon = Pokemon.query.all()
+    pokemonData = [
+        {
+            "pokemon_id": pokemon.id,
+            "attack": pokemon.attack,
+            "defense": pokemon.defense,
+            "sp_attack": pokemon.sp_attack,
+            "sp_defense": pokemon.sp_defense,
+            "speed": pokemon.speed,
+            "hp": pokemon.hp,
+            "height": pokemon.height,
+            "weight": pokemon.weight,
+            "type1": pokemon.type1,
+            "type2": pokemon.type2,
+        }
+        for pokemon in allPokemon
+    ]
+    return jsonify(pokemonData), 200
+
+
+# POST Sign Up
+@app.route("/signup", methods=["POST"])
+def signup():
+    userdata = request.json
+    if userdata is None:
+        return jsonify(message="No JSON data provided"), 400
+
+    username = userdata.get("username")
+    email = userdata.get("email")
+    password = userdata.get("password")
+
+    if not username or not email or not password:
+        return (
+            jsonify(
+                error="Invalid request. Username, email, and password are required."
+            ),
+            400,
+        )
+
+    existing_user = User.query.filter_by(username=username).first()
+    exist_email = User.query.filter_by(email=email).first()
+
+    if existing_user or exist_email:
+        return jsonify(error="username or email already exists"), 400
+
+    try:
+        new_user = User(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
+        return (
+            jsonify({"message": f"{new_user.username} created"}),
+            201,
+        )  # 201 Created status code
+
+    except IntegrityError:
+        db.session.rollback()
+        return (
+            jsonify({"message": "Username or email already exists"}),
+            400,
+        )  # 400 Bad Request status code
+
+
+# POST Log In
+@app.route("/login", methods=["POST"])
+def user_login_view():
+    data = request.json
+    response = login_user(data["username"], data["password"])
+    if not response:
+        return jsonify(message="bad username or password given"), 403
+    return response
+
+
+# POST Save my Pokemon
+@app.route("/mypokemon", methods=["POST"])
 @jwt_required()
 def myPokemon():
-  data = request.get_json()
-  pokeid = data.get('id')
-  name = data.get('name')
+    user_id = get_jwt_identity()
+    data = request.json
 
-  pokemon = Pokemon.query.get(pokeid)
-  if not pokemon:
-    return jsonify(error = f"{pokeid} is not a valid pokemon id"), 400 #bad request status code
+    if data is None:
+        return jsonify(message="No JSON data provided"), 400
 
-  
-  new_pokemon = UserPokemon(user_id=current_user.id, pokemon_id=pokemon_id, name=name)
-  db.session.add(new_pokemon)
-  db.session.commit()
-  return jsonify(message=f'{name} captured with id: {pokeid}', user_pokemon_id=new_pokemon.id), 200 #OK status code
+    pokemon_id = data.get("pokemon_id")
+    name = data.get("name")
 
-@app.route('/mypokemon', methods=['GET'])
+    if pokemon_id is None:
+        return jsonify(error="Pokemon ID is missing"), 400
+
+    pokemon = Pokemon.query.get(pokemon_id)
+    if not pokemon:
+        return jsonify(error=f"{pokemon_id} is not a valid pokemon id"), 400
+
+    new_pokemon = UserPokemon(user_id, pokemon_id, name)
+    db.session.add(new_pokemon)
+    db.session.commit()
+    return (
+        jsonify(
+            message=f"{name} captured with id: {pokemon_id}",
+            user_pokemon_id=new_pokemon.id,
+        ),
+        201,
+    )
+
+
+# GET List Pokemeon
+@app.route("/mypokemon", methods=["GET"])
 @jwt_required()
-def myListPoke():
-  pokemonData = []
-  allmypokemon = UserPokemon.query.filter_by(user_id=current_user.id).all()
-  for myPokemon in allmypokemon:
-    pokemonData.append({
-      "id": UserPokemon.id,
-      "name": UserPokemon.name,
-      "species": UserPokemon.species
-    })
-  return jsonify(pokemonData), 200
+def myListPokemon():
+    user_id = get_jwt_identity()
 
-@app.route('/mypokemon/<int:n>', methods=['GET'])
+    # get the user object of the authenticated user
+    user = UserPokemon.query.filter_by(user_id=user_id).all()
+    # converts objects to list of dictionaries
+    poke_json = [UserPokemon.get_json() for UserPokemon in user]
+    return jsonify(poke_json), 201
+
+
+# GET my Pokemon
+@app.route("/mypokemon/<int:n>", methods=["GET"])
 @jwt_required()
 def getPokemon(n):
-  pokemon = UserPokemon.query.filter_by(id=n, user_id=current_user.id).first()
-  if not pokemon:
-    return jsonify(message = f"Id {n} is invalid or does not belong to {current_user.username}"), 404 #Not found
-  return jsonify(pokemon.toDict()), 200
+    poke = UserPokemon.query.get(n)
+    # poke = UserPokemon.query.filter_by(id=n).first()
+    user_id = get_jwt_identity()
 
-@app.route('/mypokemon/<int:n>', methods=['PUT'])
+    if not poke or poke.user.name != user_id:
+        return jsonify(error=f"Id {n} is invalid or does not belong to {user_id}"), 401
+
+    return jsonify(poke.get_json()), 200
+
+
+# PUT Update Pokemon
+@app.route("/mypokemon/<int:n>", methods=["PUT"])
 @jwt_required()
-def update(n):
-  data = request.get_json()
-  new_name = data.get('name')
+def update(pokemon_id):
+    data = request.json
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(username=user_id).first()
 
-  if not new_name or new_name.strip() == '':
-    return jsonify(message='Invalid Pokemon name'), 400
+    if not user:
+        return jsonify(error=f"User {user_id} not found"), 404
 
-  pokemon = UserPokemon.query.filter_by(id=n, user_id=current_user.id).first()
-  if not pokemon:
-    return jsonify(message=f"No Pokemon captured with id {n}"), 404
+    userpoke = UserPokemon.query.filter_by(pokemon_id=pokemon_id).first()
 
-  try:
-    pokemon.name = new_name
-    db.session.commit()
-    return jsonify(message=f"{pokemon.name} renamed to {new_name}"), 200
-  except:
-    db.session.rollback()
-    return jsonify(message='Pokemon could not be named'), 500
+    if not userpoke:
+        return (
+            jsonify(
+                error=f"Id {pokemon_id} is invalid or does not belong to {user_id}"
+            ),
+            401,
+        )
 
-@app.route('/mypokemon/<int:n>', methods=['DELETE'])
+    old_name = userpoke.name
+    new_name = data.get("name")
+
+    if not new_name:
+        return jsonify(error="New name is missing"), 400
+
+    user.rename_pokemon(userpoke.pokemon_id, new_name)
+    return jsonify(message=f"{old_name} renamed to {new_name}"), 200
+
+
+# DELETE Delete Pokemon
+@app.route("/mypokemon/<int:n>", methods=["DELETE"])
 @jwt_required()
-def delete(n):
-  pokemon = UserPokemon.query.filter_by(id=n, user_id=current_user.id).first()
-  if not pokemon:
-    return jsonify(error=f"Id {n} is invalid or does not belong to {current_user.username}"), 404
+def delete(pokemon_id):
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(username=user_id).first()
 
-  try:
-    db.session.delete(pokemon)
-    db.session.commit()
-    return jsonify(message=f"{pokemon.name} released"), 200
-  except:
-    db.session.rollback()
-    return jsonify(error='Error releasing Pokemon'), 500
+    if not user:
+        return jsonify(error=f"User {user_id} not found"), 404
 
-  if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    poke = UserPokemon.query.filter_by(pokemon_id=pokemon_id).first()
 
+    if not poke:
+        return jsonify(error=f"Id {pokemon_id} is invalid or does not belong to {user_id}"), 401
+
+    pokemon_name = poke.name
+
+    user.release_pokemon(poke.id, pokemon_name)
+    return jsonify(message=f"{pokemon_name} released"), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81, debug=True)
